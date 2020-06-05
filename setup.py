804475cd8b53255284975bbc81f5266980befe02
Yonggang Wang <wixette@gmail.com>
2020-06-05 13:59:45 +0800

    separate project's README and pip package's long description

--- setup.py
+++ setup.py
@@ -1,7 +1,42 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+LONG_DESC = '''# googdiff: command-line diff tool, a wrapper of Google's diff-match-patch module.
+
+See [the API doc of Google's diff-match-patch
+module](https://github.com/google/diff-match-patch).
+
+## Install googdiff with pip
+
+```
+pip install googdiff
+```
+
+## Usage
+
+```
+# Usage info
+googdiff --help
+
+# Show diffs in console mode:
+googdiff file1 file2
+
+# or:
+googdiff -c file1 file2
+
+# Show diffs in web browser:
+googdiff -b file1 file2
+```
+
+## Git diff with googdiff
+
+```
+# in text mode:
+git difftool -y --extcmd=googdiff
+
+# or, in browser:
+git difftool -y --extcmd='googdiff -b'
+```
+'''
 
 setuptools.setup(
     name='googdiff',
@@ -9,7 +44,7 @@ setuptools.setup(
     author='wixette',
     author_email='wixette@gmail.com',
     description='A wrapper of Google\'s diff-match-patch module.',
-    long_description=long_description,
+    long_description=LONG_DESC,
     long_description_content_type="text/markdown",
     install_requires=[
         'console',
